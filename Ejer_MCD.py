87b226a4b4614852978984e5aa88ccee15444a0e
Fabian Gutierrez <fabiandavidgutierrez91@gmail.com>
2023-03-18 16:04:52 -0500

    Primos y MCD con funciones

--- Ejer_MCD.py
+++ Ejer_MCD.py
@@ -1,13 +1,14 @@
  #Dado dos números, determine el MCD (Máximo Común Divisor) entre ellos.
 
+def max_comun_div(numero1, numero2):
+    while numero2 != 0:
+        residuo = numero1 % numero2
+        numero1 = numero2
+        numero2 = residuo
+    return numero1
+
 numero1 = int(input("Ingrese su primer número: "))
 numero2 = int(input("Ingrese su segundo número: "))
-numero1_1 = numero1
-numero2_2 = numero2
 
-while numero2 != 0:
-    residuo = numero1 % numero2
-    numero1 = numero2
-    numero2 = residuo
-
-print(f"El MCD de {numero1_1} y {numero2_2} es {numero1}")
+mcd = max_comun_div(numero1, numero2)
+print(f"El MCD de {numero1} y {numero2} es {mcd}")
--- Ejer_numeros_primos.py
+++ Ejer_numeros_primos.py
@@ -1,16 +1,18 @@
 # Imprima los n números primos contenidos en un intervalo (por ejemplo los números primos del 1 al 10)
 
+def es_primo(a):
+  contador_residuo_0 = 0
+  for i in range (1, a+1):
+    if a % i == 0:
+     contador_residuo_0 += 1
+  return contador_residuo_0 == 2
+
 inicio = int(input("Ingrese inicio del intervalo: "))
 fin = int(input("Ingrese fin del intervalo: ")) 
 print(f"Los números primos del {inicio} al {fin} son: ")
 
 while fin > inicio:
-  contador_residuo_0 = 0
-  
-  for i in range(1, inicio + 1):
-     if inicio % i == 0:
-      contador_residuo_0 += 1
-  if contador_residuo_0 == 2:
+  if es_primo(inicio):
     print(inicio)
   
   inicio += 1
